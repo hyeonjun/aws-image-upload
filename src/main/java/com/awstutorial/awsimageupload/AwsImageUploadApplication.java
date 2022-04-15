fc63957f47d376a3f510774b311c56d3f07bdaa0
zlel175 <zlel175@naver.com>
2022-04-15 21:05:10 +0900

    init project

---
+++ src/main/java/com/awstutorial/awsimageupload/AwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.awstutorial.awsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadApplication.class, args);
+	}
+
+}
