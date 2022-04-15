b4f597d59b331916a9152f5922b18a796037ce67
zlel175 <zlel175@naver.com>
2022-04-15 23:11:43 +0900

    Add UserProfile Model

---
+++ spring-boot/src/main/java/com/awstutorial/awsimageupload/profile/UserProfile.java
@@ -0,0 +1,56 @@
+package com.awstutorial.awsimageupload.profile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class UserProfile {
+
+    private UUID userProfileId;
+    private String username;
+    private String userProfileImageLink; // S3 Key
+
+    public UserProfile(UUID userProfileId, String username, String userProfileImageLink) {
+        this.userProfileId = userProfileId;
+        this.username = username;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserProfileImageLink() {
+        return userProfileImageLink;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userProfileId, that.userProfileId) &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, userProfileImageLink);
+    }
+}
